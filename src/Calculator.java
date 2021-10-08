971f36b6d4cbe22fe21b155701efb6b5b851785c
dkandlhofer <dkandlhofer@know-center.at>
2021-10-08 19:58:40 +0200

    variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,20 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        
+        int a = 1;
+        System.out.println(a);
+
+        double d = 2.0;
+        System.out.println(d);
+
+        boolean b = true;
+        System.out.println(b);
+
+        char c = 'c';
+        System.out.println(c);
+
+        String s = "Ich bin ein String";
+        System.out.println(s);
     }
 
 }
