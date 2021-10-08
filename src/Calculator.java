d21c5fcda4a45a6b2ea05ed9d5168c149e92f447
dkandlhofer <dkandlhofer@know-center.at>
2021-10-08 20:22:57 +0200

    calculations

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,6 +15,29 @@ public class Calculator {
 
         String s = "Ich bin ein String";
         System.out.println(s);
+
+
+        // modulo
+        boolean even = 2 % 2 == 0;
+        System.out.println(even);
+
+        // addition
+        int add = 2 + 3;
+
+        //subtrahieren
+        int sub = 3 - 3;
+
+        //multiplizieren
+        int multi = 3 * 3;
+
+        int div = 3 / 1;
+        System.out.println(div);
+
+        int p = 2 + multi;
+        System.out.println(p);
+
+        System.out.println(4 % 2);
+        
     }
 
 }
